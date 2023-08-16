e3c3848707fead629e0a829ba08239a82babebf8
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-08-16 17:41:44 +0530

    Edit User Details

--- src/components/header/Profile.js
+++ src/components/header/Profile.js
@@ -1,54 +1,71 @@
 import React, { useRef } from "react";
 import classes from './Profile.module.css';
 import AuthContext from "../store/auth-context";
-import { useContext } from "react";
+import { useContext ,useEffect} from "react";
+import { useHistory } from 'react-router-dom';
 
 function Profile() {
+  const history = useHistory();
   const inputNameRef = useRef();
-  const inputProfileRef = useRef();
-
+  const imageInputRef = useRef();
   const ctx = useContext(AuthContext);
-console.log('ctx.idtoken',ctx.idtoken)
-  const submitHandler = (event) => {
+  console.log('ctx.idtoken',ctx.idtoken);
+ // "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAjnoOONhbal4-8ybz17138o-5U25hPyxM",
+
+
+
+  const submitHandler = async (event) => {
     event.preventDefault();
-
+  
     const enteredName = inputNameRef.current.value;
-    const enteredProfile = inputProfileRef.current.value;
+    const imageUrl = imageInputRef.current.value;
 
-    //optional:add validation
+     // Save data to localStorage
+     localStorage.setItem('enteredName', enteredName);
+     localStorage.setItem('imageUrl', imageUrl);
 
-    fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAjnoOONhbal4-8ybz17138o-5U25hPyxM",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          idToken: ctx.idToken,
-          displayName: enteredName,
-          photoUrl: enteredProfile,
-          returnSecureToken: true,
-        }),
-      }
-    )
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          return res.json().then((data) => {
-            console.log(data);
-            if (data.error.message) {
-              alert(data.error.message);
-            }
-          });
+    console.log(enteredName);
+    console.log(imageUrl);
+  
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAjnoOONhbal4-8ybz17138o-5U25hPyxM",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: ctx.idtoken,
+            displayName: enteredName,
+            photoUrl: imageUrl,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type' : 'application/json',
+          },
         }
-      })
-      .then((data) => {
-        console.log("recieved data", data);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("failed to update profile");
-      });
+      );
+    
+      const data = await response.json(); // Parse the response body once
+    
+      if (response.ok) {
+        ctx.login(data.idToken);
+        history.replace('/welcome');
+        console.log('then', data);
+      } else {
+        let errorMessage = "Authentication failed";
+        if (data && data.error && data.error.message) {
+          errorMessage = data.error.message;
+        }
+        throw new Error(errorMessage);
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+    
   };
+  
+
+      
+
 
   return (
     <section className={classes.head}>
@@ -68,13 +85,13 @@ console.log('ctx.idtoken',ctx.idtoken)
           <label htmlFor="name"> Full Name</label>
           <input type="text" id="name" required ref={inputNameRef} />
           <label htmlFor="profile">Profile Photo URL</label>
-          <input type="url" id="url" required ref={inputProfileRef} /> <br />
+          <input type="url" id="url" required ref={imageInputRef} /> <br />
           <br />
           <button type="submit">Update</button>
         </form>
       </div>
     </section>
   );
-}
+};
 
 export default Profile;
--- src/components/header/Welcome.js
+++ src/components/header/Welcome.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Welcome.module.css";
-import { NavLink } from "react-router-dom"; // Import NavLink
+import { NavLink } from "react-router-dom";
 import { Fragment } from "react";
+import AuthContext from "../store/auth-context";
 
 function Welcome() {
+  const storedName = localStorage.getItem("enteredName");
+  const storedImageUrl = localStorage.getItem("imageUrl");
+  const isProfileIncomplete = storedName || storedImageUrl;
+  console.log(isProfileIncomplete)
+
   return (
     <Fragment>
       <div className={classes.main}>
-        <div>Welcome to expense tracker!!!!</div>
-        <div className={classes.right}>
-          Your profile is incomplete.
-          <NavLink to="/welcome/profile">Complete now</NavLink>
-        </div>
+        <div>Welcome to the expense tracker!!!!</div>
+        {!isProfileIncomplete && <div className={classes.right}>     
+            <div>
+              Your profile is incomplete.
+              <NavLink to="/welcome/profile">Complete now</NavLink>
+            </div>
+        </div> }
+        <div>Welcome, {storedName}</div>
       </div>
     </Fragment>
   );
