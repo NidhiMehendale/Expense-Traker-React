ea4d5887851c8a5495550b5ccef6a5aec187cb01
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-08-22 17:03:17 +0530

    Integrating Redux with react

--- redux-demo.js
+++ redux-demo.js
@@ -1,17 +1,16 @@
 const redux = require('redux')
 
 const counterReducer = (state = { counter : 0},action) => {
-  if (action.type === "increment") {
+  if(action.type==='INCREMENTBY2'){
     return {
-      counter: state.counter + 5,
+        counter:state.counter + 2
     };
-  }
-
-  if (action.type === "decrement") {
+}
+if(action.type==='DECREMENTBY2'){
     return {
-      counter: state.counter - 1,
-    };
-  }
+        counter:state.counter-2
+    }
+}
 
   return state;
 
@@ -26,5 +25,6 @@ const counterSubscriber = () => {
   };
 
   store.subscribe(counterSubscriber);
-  store.dispatch({type : 'increment'});
-  store.dispatch({ type: "decrement" });
+  
+  store.dispatch({ type:'INCREMENTBY2' })
+  store.dispatch({type:'DECREMENTBY2'})
