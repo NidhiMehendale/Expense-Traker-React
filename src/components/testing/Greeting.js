dc6df406b7fc150bfba274d6db980dff443058c4
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-08-31 15:17:07 +0530

    Testing User Actions

--- src/components/testing/Greeting.js
+++ src/components/testing/Greeting.js
@@ -1,11 +1,21 @@
-function Greeting() {
+import { useState } from 'react';
+import Output from './Output';
+
+const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      <p>It's good to see you!</p>
+      <h2>Hello World!</h2>
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
-}
+};
 
 export default Greeting;
